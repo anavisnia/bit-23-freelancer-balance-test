f945d08398ab2d45d6614fa74402f1dce41341f9
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-29 20:39:46 +0200

    render data, no balance

--- js/components/renderTable.js
+++ js/components/renderTable.js
@@ -1,15 +1,20 @@
+import { menesiai } from "../data.js";
+
 function generateTable(selector, data) {
     const DOM = document.querySelector(selector);
     let HTML = '';
+    for(let i = 0; i < data.length; i++) {
+        const monthStat = data[i];
 
-    HTML =                 `<div class="table-row">
-                                <div class="cell">1</div>
-                                <div class="cell">Sausis</div>
-                                <div class="cell">150.00 Eur</div>
-                                <div class="cell">-</div>
-                                <div class="cell">150.00 Eur</div>
-                            </div>`;
-    DOM.innerHTML = HTML;
+        HTML = `<div class="table-row">
+                    <div class="cell">${monthStat.month}</div>
+                    <div class="cell">${menesiai[ monthStat.month-1 ]}</div>
+                    <div class="cell">${monthStat.income ? monthStat.income + ' Eur' : '-'}</div>
+                    <div class="cell">${monthStat.expense ? monthStat.expense + ' Eur' : '-'}</div>
+                    <div class="cell">150.00 Eur / Balansas</div>
+                </div>`;
+        DOM.innerHTML += HTML;
+    }
 }
 
 export { generateTable }
